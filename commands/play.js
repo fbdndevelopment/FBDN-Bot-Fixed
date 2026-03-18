dc7bb31f75d9d68523708caefc4eaa7bda5f6ab3
fbdndevelopment <ghoulremixes@gmail.com>
2026-03-18 22:53:50 +0000

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -8,7 +8,7 @@ module.exports = {
     if (!query) return message.reply("❌ Provide a song name!");
 
     try {
-      await client.play-dl.play(voiceChannel, query, {
+      await client.ply-dl.play(voiceChannel, query, {
         member: message.member,
         textChannel: message.channel
       });
